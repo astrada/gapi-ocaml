ca3673e8803195ca79df29ebaec6debf13976c33
Alessandro Strada <a.strada@ex-nunc.org>
2011-11-05 13:24:15 +0100

    Use atom feed functor for comments feed

--- src/gdata/gdataComments.mli
+++ src/gdata/gdataComments.mli
@@ -1,47 +1,70 @@
 module Make :
   functor (Link : GdataCore.DATA) ->
 sig
-  type commentEntry = {
-    ce_etag : string;
-    ce_authors : GdataAtom.atom_author list;
-    ce_content : GdataAtom.atom_content;
-    ce_contributors : GdataAtom.atom_contributor list;
-    ce_id : GdataAtom.atom_id;
-    ce_published : GdataAtom.atom_published;
-    ce_updated : GdataAtom.atom_updated;
-    ce_links : Link.t list;
-    ce_title : GdataAtom.atom_textConstruct;
-    ce_category : GdataAtom.atom_category;
-    ce_extensions : GdataCore.xml_data_model list
-  }
+  module Entry :
+  sig
+    type t = {
+      ce_etag : string;
+      ce_authors : GdataAtom.atom_author list;
+      ce_content : GdataAtom.atom_content;
+      ce_contributors : GdataAtom.atom_contributor list;
+      ce_id : GdataAtom.atom_id;
+      ce_published : GdataAtom.atom_published;
+      ce_updated : GdataAtom.atom_updated;
+      ce_links : Link.t list;
+      ce_title : GdataAtom.atom_textConstruct;
+      ce_category : GdataAtom.atom_category;
+      ce_extensions : GdataCore.xml_data_model list
+    }
 
-  val empty_commentEntry : commentEntry
+    val empty : t
 
-  type commentFeed = {
-    cf_etag : string;
-    cf_authors : GdataAtom.atom_author list;
-    cf_contributors : GdataAtom.atom_contributor list;
-    cf_generator : GdataAtom.atom_generator;
-    cf_id : GdataAtom.atom_id;
-    cf_updated : GdataAtom.atom_updated;
-    cf_entries : commentEntry list;
-    cf_links : Link.t list;
-    cf_title : GdataAtom.atom_textConstruct;
-    cf_category : GdataAtom.atom_category;
-    cf_itemsPerPage : GdataAtom.opensearch_itemsPerPage;
-    cf_startIndex : GdataAtom.opensearch_startIndex;
-    cf_totalResults : GdataAtom.opensearch_totalResults;
-    cf_extensions : GdataCore.xml_data_model list
-  }
+    val of_xml_data_model : t -> GdataCore.xml_data_model -> t
 
-  val empty_commentFeed : commentFeed
+    val to_xml_data_model : t -> GdataCore.xml_data_model list
+
+  end
+
+  module Feed :
+  sig
+    type t = GdataAtom.MakeFeed(Entry)(Link).t = {
+      f_etag : string;
+      f_kind : string;
+      f_authors : GdataAtom.atom_author list;
+      f_categories : GdataAtom.atom_category list;
+      f_contributors : GdataAtom.atom_contributor list;
+      f_generator : GdataAtom.atom_generator;
+      f_icon : GdataAtom.atom_icon;
+      f_id : GdataAtom.atom_id;
+      f_updated : GdataAtom.atom_updated;
+      f_entries : Entry.t list;
+      f_links : Link.t list;
+      f_logo : GdataAtom.atom_logo;
+      f_rights : GdataAtom.atom_textConstruct;
+      f_subtitle : GdataAtom.atom_textConstruct;
+      f_title : GdataAtom.atom_textConstruct;
+      f_totalResults : GdataAtom.opensearch_totalResults;
+      f_itemsPerPage : GdataAtom.opensearch_itemsPerPage;
+      f_startIndex : GdataAtom.opensearch_startIndex;
+      f_extensions : GdataCore.xml_data_model list
+    }
+
+    val empty : t
+
+    val of_xml_data_model : t -> GdataCore.xml_data_model -> t
+
+    val to_xml_data_model : t -> GdataCore.xml_data_model list
+
+    val parse_feed : GdataCore.xml_data_model -> t
+
+  end
 
   type comments = {
     c_countHint : int;
     c_href : string;
     c_readOnly : bool;
     c_rel : string;
-    c_commentFeed : commentFeed;
+    c_commentFeed : Feed.t;
   }
 
   val empty_comments : comments
@@ -51,16 +74,12 @@ sig
     GdataCore.xml_data_model ->
     comments
 
-  val parse_comment_feed :
-    GdataCore.xml_data_model ->
-    commentFeed
-
   val parse_comment_entry :
     GdataCore.xml_data_model ->
-    commentEntry
+    Entry.t
 
   val comment_entry_to_data_model :
-    commentEntry ->
+    Entry.t ->
     GdataCore.xml_data_model
 
   val render_comments :
