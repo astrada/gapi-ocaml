74bbb195f393d69673dc99142a5499b585522126
Alessandro Strada <alessandro.strada@gmail.com>
2011-11-26 15:34:11 +0100

    Add GapiJson module interface

---
+++ src/gapi/gapiJson.mli
@@ -0,0 +1,93 @@
+type json_data_type =
+    Object
+  | Array
+  | Scalar
+
+val json_data_type_to_string : json_data_type -> string
+
+type json_metadata = {
+  name : string;
+  data_type : json_data_type
+}
+
+val metadata_description : json_metadata -> string
+
+type json_data_model = (json_metadata, Json_type.t) GapiCore.AnnotatedTree.t
+
+val unexpected : string -> json_data_model -> 'a
+
+val render_value :
+  string ->
+  Json_type.t ->
+  Json_type.t ->
+  json_data_model list
+
+val render_string_value :
+  ?default:string ->
+  string ->
+  string ->
+  json_data_model list
+
+val render_int_value :
+  ?default:int ->
+  string ->
+  int ->
+  json_data_model list
+
+val render_bool_value :
+  ?default:bool ->
+  string ->
+  bool ->
+  json_data_model list
+
+val render_struct :
+  string ->
+  json_data_type ->
+  json_data_model list list ->
+  json_data_model list
+
+val render_object :
+  string ->
+  json_data_model list list ->
+  json_data_model list
+
+val render_array :
+  string ->
+  ('a -> json_data_model list) ->
+  'a list ->
+  json_data_model list
+
+val parse_children :
+  ('a -> json_data_model -> 'a) ->
+  'a ->
+  ('a -> 'b) ->
+  json_data_model list ->
+  'b
+
+val parse_array :
+  ('a -> json_data_model -> 'a) ->
+  'a ->
+  ('a list -> 'b) ->
+  json_data_model list ->
+  'b
+
+val json_to_data_model :
+  Json_type.json_type ->
+  json_data_model
+
+val data_model_to_json :
+  json_data_model ->
+  Json_type.json_type
+
+val parse_json_response :
+  (json_data_model -> 'a) ->
+  GapiPipe.OcamlnetPipe.t ->
+  'a
+
+val default_content_type : string
+
+val render_json :
+  ('a -> json_data_model) ->
+  'a ->
+  GapiCore.PostData.t
+
