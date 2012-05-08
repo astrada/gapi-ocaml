bb9d92465507c4adc677943d214930443371e80a
Alessandro Strada <alessandro.strada@gmail.com>
2012-05-08 22:39:50 +0200

    Fix test of revisions feed

--- src/test/testDocumentsV3Model.ml
+++ src/test/testDocumentsV3Model.ml
@@ -40,8 +40,8 @@ let test_parse_revisions_feed () =
   let ch = open_in "test_data/test_documents_revisions_feed.xml" in
   let feed = GdataUtils.parse_xml
                 (fun () -> input_byte ch)
-                Document.Feed.parse_feed in
-  let tree = Document.document_feed_to_data_model feed in
+                Revision.Feed.parse_feed in
+  let tree = Revision.revisions_feed_to_data_model feed in
     TestHelper.assert_equal_file
       "test_data/test_parse_revisions_feed.xml"
       (GdataUtils.data_to_xml_string tree)
