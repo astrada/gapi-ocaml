f875051353994c87ae02a532cd3a7a0a9ad2d8bc
Alessandro Strada <alessandro.strada@gmail.com>
2017-01-05 19:06:45 +0100

    Update version to 0.3.2

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,5 +1,5 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: d03497f71000099bd0658e18af86ab07) *)
+(* DO NOT EDIT (digest: a2bb17f5fa66dc361c9088dcd14bc5ab) *)
 module OASISGettext = struct
 (* # 22 "src/oasis/OASISGettext.ml" *)
 
@@ -746,6 +746,9 @@ module MyOCamlbuildBase = struct
 (* # 110 "src/plugins/ocamlbuild/MyOCamlbuildBase.ml" *)
 
 
+  let env_filename = Pathname.basename BaseEnvLight.default_filename
+
+
   let dispatch_combine lst =
     fun e ->
       List.iter
@@ -878,7 +881,7 @@ module MyOCamlbuildBase = struct
 end
 
 
-# 881 "myocamlbuild.ml"
+# 884 "myocamlbuild.ml"
 open Ocamlbuild_plugin;;
 let package_default =
   {
@@ -900,6 +903,6 @@ let conf = {MyOCamlbuildFindlib.no_automatic_syntax = false}
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default conf package_default;;
 
-# 904 "myocamlbuild.ml"
+# 907 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
--- src/gapi/gapiCore.ml
+++ src/gapi/gapiCore.ml
@@ -1,4 +1,4 @@
-let library_version = "0.3.0"
+let library_version = "0.3.2"
 
 module AnnotatedTree = struct
   type ('a, 'b) t =
