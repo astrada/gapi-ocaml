446bba81a50ec9ab9e03405c0e6d30df75e9e91a
Alessandro Strada <alessandro.strada@gmail.com>
2019-09-17 20:32:35 +0200

    Fix ocamldoc comments

--- src/gapi/gapiOAuth2ServiceAccount.mli
+++ src/gapi/gapiOAuth2ServiceAccount.mli
@@ -2,21 +2,22 @@
   *)
 
 (**
- * [get_access_token path scopes]: returns the access token for the service
- * account. [service_account_data_path] is the path of the JSON file
- * containing the account data (client id, private key, etc.), [scopes] is a
- * list of the permissions that the application requests.
- *
- * @param url URL of the auth endpoint (defaults to
- * https://www.googleapis.com/oauth2/v4/token).
- * @param assertion_timestamp The time (Unix Epoch) the assertion is issued
- * (defaults to now).
- * @param assertion_expiration The time (Unix Epoch) the assertion expires
- * (defaults to now + 1 hour).
- * @param user_to_impersonate Email of the user to impersonate (for G Suite
- * domains).
- *
- * @see https://developers.google.com/identity/protocols/OAuth2ServiceAccount *)
+ [get_access_token path scopes]: returns the access token for the service
+ account. [service_account_data_path] is the path of the JSON file
+ containing the account data (client id, private key, etc.), [scopes] is a
+ list of the permissions that the application requests.
+ 
+ @param url URL of the auth endpoint (defaults to
+ https://www.googleapis.com/oauth2/v4/token).
+ @param assertion_timestamp The time (Unix Epoch) the assertion is issued
+ (defaults to now).
+ @param assertion_expiration The time (Unix Epoch) the assertion expires
+ (defaults to now + 1 hour).
+ @param user_to_impersonate Email of the user to impersonate (for G Suite
+ domains).
+ 
+ @see <https://developers.google.com/identity/protocols/OAuth2ServiceAccount>
+*)
 val get_access_token :
   ?url:string ->
   ?assertion_timestamp:float ->
